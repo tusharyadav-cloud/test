3a573bfbf2893cb0baa2eab4d41dbc95b04cc6c9
Tushar01yadav <tusharyadav61900@gmail.com>
2026-01-29 16:05:12 +0530

    try

--- calculator.py
+++ calculator.py
@@ -1,12 +1,1 @@
-#feature-mine
-def add(a,b):
-    return a + b
-
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: ")) 
-print("The sum is:", add(a,b)) 
-
-def subtract(a,b):
-    return a +b
-
-print("The difference is:", subtract(a,b))
+#updated 
